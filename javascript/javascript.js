eeae70386556733f947f9e7a3954c5fd6d96eb6a
Malvuna <3405635@gmail.com>
2022-06-20 17:16:33 +0300

    fix duplication

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,5 @@
 import data from "./emoji.js";
+const dataUniq = changeUniqKeywords();
 
 let main = document.querySelector("main");
 main.className = "main";
@@ -28,29 +29,42 @@ function createElem(symbol, title, keywords) {
   div.append(description);
 }
 
-data.forEach((elem) => {
-    createElem(elem.symbol, elem.title, elem.keywords);
-  });
+dataUniq.forEach((elem) => {
+  createElem(elem.symbol, elem.title, elem.keywords);
+});
 
 let listElem = document.querySelectorAll(".divCard");
 
 function inputInput() {
-    let value = input.value;
-    for (let item of listElem) {
-      if (value !== item.querySelector(".divCard__titel").textContent) {
-        item.style.display = "none";
-      }
-    }
-  };
+  let value = input.value;
+  let filterEmoji = dataUniq.filter((elem) => elem.title.includes(value));
+  console.log(filterEmoji);
 
-  input.addEventListener ('change', inputInput )
+  main.innerHTML = "";
 
-  
-  // input.onchange = function () {
-  //   let value = input.value;
-  //   for (let item of listElem) {
-  //     if (value !== item.querySelector(".divCard__titel").textContent) {
-  //       item.style.display = "none";
-  //     }
-  //   }
-  // };
+  filterEmoji.forEach((elem) => {
+    createElem(elem.symbol, elem.title, elem.keywords);
+  });
+}
+
+input.addEventListener("input", inputInput);
+
+function changeUniqKeywords() {
+  return data.map((elem) => {
+    return {
+      ...elem,
+      keywords: [...new Set(elem.keywords.split(" "))].join([" "]),
+    };
+  });
+}
+
+// console.log(data);
+
+// for (let i = 0; i < 1; i++) {
+//   let test = data[i];
+//   let testTwo = test.keywords.split(" ");
+//   const set1 = new Set(testTwo);
+//   let set2 = [...set1];
+//   let testThree = set2.join([" "]);
+//   let result = { ...data[i], keywords: testThree };
+// }
